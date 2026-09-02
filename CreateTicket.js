6c0596222f353305c6043bbb22d3a73a667f770b
Rohit0332 <rksingh03229@gmail.com>
2026-09-02 13:40:31 +0530

    Add files via upload

---
+++ CreateTicket.js
@@ -0,0 +1,103 @@
+import React from 'react';
+
+ function CreateTicket() {
+    return (
+    <div className='container'>
+      <div className='row p-5 mt-5'>
+
+     <h1 className='fs-2 text-center border-bottom text-center '>
+        To Create a Ticket , select a relevant topic  </h1><br/>
+        < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true">Account Opening </i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}>  Online Opening Account</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Off line Opening Account   </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Company , Partnership Account </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Opening  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5",lineHeight:"2.5",lineHeight:"2.5"}}>  For NRI Account  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Charges at Zerodha </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Zerodha IDFC FIRST Bank in Account   </a><br/>  
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Getting Started  </a><br/>  
+  
+  </div>
+  < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true"> Your Zerodha Account</i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}>  Login Credentials</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}>  Account Segementation Addition   </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> ID and Bank Details </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Your Profile  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5",lineHeight:"2.5",lineHeight:"2.5"}}>  Transfer and Conversation of share  </a>  <br/>
+  
+  </div>
+  < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true">Your Zerodha Account </i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}>  Products and Order Types</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Kite Web </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Trading FAQs </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}>Cooperate Actions </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5",lineHeight:"2.5",lineHeight:"2.5"}}>  Sentinel </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Kite API </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> PI and Other  </a><br/>  
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Stocks details </a><br/>  
+  </div>
+  < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true">Funds </i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}> Adding Funds</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Adding Bank in Account  </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Company , Partnership Account </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Opening  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Zerodha IDFC FIRST Bank in Account   </a><br/>  
+  
+  </div>
+  
+  < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true">Console </i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}> Reports</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Ledger   </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> portfolio </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> 60  day Challenges  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5",lineHeight:"2.5",lineHeight:"2.5"}}>    </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Charges at Zerodha </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Zerodha IDFC Account   </a><br/>  
+  
+  </div>
+  
+  < div className='col-4 '>
+        <h4>
+            <i class="fa fa long-arrow-right" aria-hidden="true">coin </i>
+        </h4><br/>
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5 "}}>  About coin</a>  <br/>
+  
+         <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Off line coinAccount   </a> <br/> 
+          <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Company Account </a>  <br/>
+           <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> coin Opening  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5",lineHeight:"2.5",lineHeight:"2.5"}}>  For NRI Account  </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> coin Charges at Zerodha </a>  <br/>
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Zerodha IDFC FIRST Bank in Account   </a><br/>  
+   <a href='' style={{textDecoration:"none",lineHeight:"2.5"}}> Getting Started  </a><br/>  
+  
+  </div>
+  
+      </div>
+
+       
+
+    </div>   );
+}
+
+export default CreateTicket;
+        
--- Hero.js
+++ Hero.js
@@ -1,18 +1,23 @@
-
 import React from 'react';
-function Hero() {
-    return (  
-       <div className='container p-5'>
-         <div className='row text-center'>
-            <img src='Images/images/homeHero.png' alt='Hero Image 'className='mb-5'/>
-            <h1 className='mt-5'>Invest in Everything</h1>
-            <p> Online Platform to invest in stocks, derivates and </p>
-            <button  className='p-3 btn btn-danger fs-5 mb-5' style={{width:"25%",margin:"0 auto"}}>Signup</button>
 
-         </div>
+ function Hero() {
+    return (
+        <section className=" container " id ="supportHero">
+<div className='row ' id='supports'>
+    <h4>SupportPage</h4>
+    <a href='' style={{textDecoration:"none"}}> Track Ticket </a>
+   
+    </div> 
+    
+    <div className='row ' id='supports'>
+    <h4>SupportPage</h4>
+      
+    </div> 
+    
 
-       </div>
-    );
+        </section>
+      );
 }
 
 export default Hero;
+        
